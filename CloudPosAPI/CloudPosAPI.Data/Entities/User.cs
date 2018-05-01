c740c1e0665f2135a916d19ce16a8ef927293ed0
Ranga Nirmana <rnirmana@gmail.com>
2018-05-02 01:02:13 +0530

    Setting up basic configurations

--- CloudPosAPI/CloudPosAPI.Data/Entities/User.cs
+++ CloudPosAPI/CloudPosAPI.Data/Entities/User.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CloudPosAPI.Data.Entities
+﻿namespace CloudPosAPI.Data.Entities
 {
     class User
     {
-        /test
+       //test
     }
 }
---
+++ CloudPosAPI/CloudPosAPI.Dto/Enum/SystemResponseCode.cs
@@ -0,0 +1,10 @@
+﻿namespace CloudPosAPI.Dto.Enum
+{
+    public enum SystemResponseCode
+    {
+        OK = 0,
+        VALIDATIONFAILED = -1,
+        UNKNOWNERROR=-2
+
+    }
+}
