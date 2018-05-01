a576f0a9b7780326245c5d1d86b58209dfcbcaa6
chirantha_mdk <chirantha_mdk@susl.lk>
2018-05-01 18:25:27 +0530

    Instituitions Correction

--- CloudPosAPI/CloudPosAPI.Data/Entities/Institution.cs
+++ CloudPosAPI/CloudPosAPI.Data/Entities/Institution.cs
@@ -10,14 +10,14 @@ namespace CloudPosAPI.Data.Entities
         public string Adress1 {get; set; }
         public string Adress2 {get; set; }
         public Guid CityId {get; set; }
-        public Guid CountryID {get; set; }
+        public Guid CountryId {get; set; }
         public Guid CurrencyId {get; set; }
         public string Language {get; set; }
         public string TimeZone {get; set; }
         public string ContractPerson {get; set; }
         public string Email {get; set; }
-        public string TelephoneNO {get; set;}
+        public string TelephoneNo {get; set; }
         public byte[] Logo {get; set; }
-        public int? IsActive {get; set; }        
+        public bool IsActive {get; set; }        
     }
 }
