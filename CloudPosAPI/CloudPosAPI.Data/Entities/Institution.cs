8b3c452a37bca57fd4f0228141eaef123afc1dc4
chirantha_mdk <chirantha_mdk@susl.lk>
2018-05-01 09:42:56 +0530

    Add Institution Entity

--- CloudPosAPI/CloudPosAPI.Data/Entities/Institution.cs
+++ CloudPosAPI/CloudPosAPI.Data/Entities/Institution.cs
@@ -6,7 +6,18 @@ namespace CloudPosAPI.Data.Entities
     {
         public Guid Id { get; set; }
         public string Code { get; set; }
-        //todo:
-
+        public string Name {get; set; }
+        public string Adress1 {get; set; }
+        public string Adress2 {get; set; }
+        public Guid CityId {get; set; }
+        public Guid CountryID {get; set; }
+        public Guid CurrencyId {get; set; }
+        public string Language {get; set; }
+        public string TimeZone {get; set; }
+        public string ContractPerson {get; set; }
+        public string Email {get; set; }
+        public string TelephoneNO {get; set;}
+        public byte[] Logo {get; set; }
+        public int? IsActive {get; set; }        
     }
 }
